d45179f46b09722777bc0ea825358a5794cbdc44
edapess <edmondavetisyanw@gmail.com>
2023-09-01 17:46:32 +0400

    feat: init insert num to array or search its index

---
+++ src/search_insert_position.ts
@@ -0,0 +1,21 @@
+function searchInsert(nums: number[], target: number): number {
+  let left = 0; //left index
+  let right = nums.length - 1; //right index
+  
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid; 
+    }
+    if(target < nums[mid]){
+      right = mid -1
+    }else{
+      left = mid + 1
+    }
+  }
+  return left;
+}
+
+
+ searchInsert([1,3,5,6], 5)
+
