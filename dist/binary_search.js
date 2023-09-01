34a33b60ac84e029e279c11e20c5d24984c0a9ee
edapess <edmondavetisyanw@gmail.com>
2023-09-01 14:15:40 +0400

    feat: init binary search

---
+++ dist/binary_search.js
@@ -0,0 +1,29 @@
+"use strict";
+function search(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+console.log(search([-1, 0, 3, 5, 9, 12], 9));
