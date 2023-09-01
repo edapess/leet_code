fb0b7632218b4cddc6a47aa2a0da583876276abc
edapess <edmondavetisyanw@gmail.com>
2023-09-01 19:13:13 +0400

    feat: smallest character

---
+++ src/smallest_letter.ts
@@ -0,0 +1,16 @@
+function nextGreatestLetter(letters: string[], target: string): string {
+  let leftIndex = 0;
+  let rightIndex = letters.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    const middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (letters[middleIndex] <= target) {
+      leftIndex = middleIndex + 1;
+    } else if (letters[middleIndex] > target) {
+      rightIndex = middleIndex - 1;
+    }
+  }
+  return leftIndex === letters.length ? letters[0] : letters[leftIndex];
+}
+
+console.log(nextGreatestLetter(["c", "f", "j"], "c"));
